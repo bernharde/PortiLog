b777ea1d369bf61b1cd0a2b82c349154520b7554
bernharde <bernhardelbl@hotmail.com>
2014-08-06 12:34:37 +0200

    rest service test

--- sources/PortiLog.Service.Tests/UnitTest1.cs
+++ sources/PortiLog.Service.Tests/UnitTest1.cs
@@ -20,55 +20,10 @@ namespace PortiLog.Service.Tests
             dumpData.ApplicationVersion = "0.1";
             dumpData.Entries = new System.Collections.Generic.List<Entry>();
             dumpData.Entries.Add(new Entry() { Id = 1, Created = DateTime.Now, Message = "my message123" });
-            await PostAsync<object, DumpData>(null, dumpData);        
+            var client = new ServiceClient("http://localhost/logging2");
+            await client.PostDumpData(dumpData);     
         }
 
-        public static async Task<T> JsonDeserializeObjectAsync<T>(string sValue)
-        {
-            var result = await Task.Factory.StartNew<T>(() => { return JsonConvert.DeserializeObject<T>(sValue); });
-            return result;
-        }
-
-        public static async Task<string> JsonSerializeObjectAsync<T>(T value)
-        {
-            var result = await Task.Factory.StartNew<string>(() => { return JsonConvert.SerializeObject(value); });
-            return result;
-        }
-
-        public async Task<T> PostAsync<T, PT>(string token, PT tPostData)
-        {
-            var postData = await JsonSerializeObjectAsync(tPostData);
-            var result = await PostAsync<T>(token, postData);
-            return result;
-        }
-
-        public async Task<T> PostAsync<T>(string token, string postData)
-        {
-            try
-            {
-                using (var client = new HttpClient())
-                {
-                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                    //var url = await CreateUrlAsync(token);
-                    var url = "http://localhost/logging2/api/logging";
-                    
-                    var decodedUrl = Uri.EscapeUriString(url);
-                    StringContent postContent = new StringContent(postData, Encoding.UTF8, "application/json");
-                    using (var response = await client.PostAsync(decodedUrl, postContent))
-                    {
-                        response.EnsureSuccessStatusCode();
-                        var content = await response.Content.ReadAsStringAsync();
-                        var value = await JsonDeserializeObjectAsync<T>(content);
-                        return value;
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-                //Logger.Error("ServerService_PostAsync failed", ex);
-                //throw new Models.ServerException(strings.ServerError, ex);
-            }
-        }
+       
     }
 }
